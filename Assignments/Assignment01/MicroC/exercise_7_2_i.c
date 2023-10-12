44ff3258fb12cf6836dd0cf5ac62a829b24130ed
Adam Hadou Temsamani <ahad@itu.dk>
2023-10-12 12:55:38 +0200

    Exercise 7.2 Done

---
+++ Assignments/Assignment01/MicroC/exercise_7_2_i.c
@@ -0,0 +1,23 @@
+﻿void main(int n) {
+    int arr[4];
+    arr[0] = 7;
+    arr[1] = 13;
+    arr[2] = 9;
+    arr[3] = 8;
+    
+    int sump;
+    arrsum(n, arr, &sump);
+    print sump; 
+}
+
+
+void arrsum(int n, int arr[], int *sump) {
+    int i;
+    i = 0;
+    while (i < n) {
+        *sump = *sump + arr[i]; 
+        i = i + 1;
+    }
+}
+
+
---
+++ Assignments/Assignment01/MicroC/exercise_7_2_iii.c
@@ -0,0 +1,46 @@
+﻿void main(int n) {
+
+    int freq[7];
+    freq[0] = 0;
+    freq[1] = 0;
+    freq[2] = 0;
+    freq[3] = 0;
+    freq[4] = 0;
+    freq[5] = 0;
+    freq[6] = 0;
+    
+    int ns[7];
+    
+    ns[0] = 1;
+    ns[1] = 2;
+    ns[2] = 1;
+    ns[3] = 1;
+    ns[4] = 1;
+    ns[5] = 2;
+    ns[6] = 0;
+
+    int max;
+    max = 6;
+    histogram(n,ns, max, freq);
+}
+
+void histogram(int n, int ns[], int max, int freq[]) {
+    int i;
+    i = 0;
+    while (i < n) {
+        int tmp;
+        tmp = ns[i];
+        freq[tmp] = freq[tmp] + 1;
+        i = i + 1;
+    }
+
+    int j;
+    j = 0;
+    while(j < max) {
+        print freq[j];
+        j = j + 1;
+    }
+}
+
+
+
