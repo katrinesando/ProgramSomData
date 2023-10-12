59df298a998ee8e4fa054544ed766fec5ab88e3f
Adam Hadou Temsamani <ahad@itu.dk>
2023-10-12 13:57:46 +0200

    Create exercise_7_3_i.c

---
+++ Assignments/Assignment01/MicroC/exercise_7_3_i.c
@@ -0,0 +1,20 @@
+﻿void main(int n) {
+    int arr[4];
+    arr[0] = 7;
+    arr[1] = 13;
+    arr[2] = 9;
+    arr[3] = 8;
+    
+    int sump;
+    arrsum(n, arr, &sump);
+    print sump; 
+}
+
+
+void arrsum(int n, int arr[], int *sump) {
+    int i;
+    for (i = 0; i < n; i = i+1)
+        *sump = *sump + arr[i]; 
+}
+
+
